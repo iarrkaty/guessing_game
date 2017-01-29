d0c28f0de45bfcc83b160cbb1f49390acc6c06d8
‘Katy’ <katyinseattle@me.com>
2017-01-29 10:36:13 -0800

    Sunday2

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ console.log(userName);
 
 var answerOne = prompt('What country was I born in?');
 var myAnswerOne ='Japan';
+console.log(answerOne);
 
 if(answerOne === myAnswerOne) {
-  alert('YAY! You got it right!')
+  alert('You got it right!')
 } else {
   alert('Sorry you got it wrong!')
 }
 
 var answerTwo = prompt('What publications has my work been published in?');
 var myAnswerTwo = 'New Yorker and MacWorld';
+console.log(answerTwo);
 
 if(answerTwo === myAnswerTwo) {
   alert('You got it right!')
@@ -24,6 +26,7 @@ if(answerTwo === myAnswerTwo) {
 
 var answerThree = prompt('What was my first job?');
 var myAnswerThree = 'Custom t-shirt shop';
+console.log(answerThree);
 
 if(answerThree === myAnswerThree) {
   alert('You got it right!')
@@ -32,6 +35,7 @@ if(answerThree === myAnswerThree) {
 }
 var answerFour = prompt('What is my favorite show?');
 var myAnswerFour = 'Doctor Who';
+console.log(answerFour);
 
 if(answerFour === myAnswerFour) {
   alert('You got it right!')
@@ -39,6 +43,12 @@ if(answerFour === myAnswerFour) {
   alert('Sorry you got it wrong!')
 }
 
+var answerFive = prompt('What do I collect?');
+var answerFive = ('Records' , 'Tattoos' , 'Cookbooks');
+console.log(answerFive);
 
+var answerSix = prompt('How old am I?');
+var myAnswerSix = '28'
+console.log(answerSix);
 
 }
